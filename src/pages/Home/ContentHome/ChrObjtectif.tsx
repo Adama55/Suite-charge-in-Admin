03b52d8f571327c921715140a193f92430b3951b
Adama55 <mafahfof55@gmail.com>
2023-09-11 08:28:29 +0200

    composant objectif

--- src/pages/Home/ContentHome/ChrObjtectif.tsx
+++ src/pages/Home/ContentHome/ChrObjtectif.tsx
@@ -7,39 +7,43 @@ function ChrObjtectif() {
 
 	const location = useLocation()
 	return <div className='sl-chrObjtectif gradient-lineare-seconadry'>
-		<div className='container d-flex flex-column flex-xxl-row'>
-			<div className='text-poppins sl-text-head'>
+		<div className='container list-unstyled'> {/*d-flex flex-column flex-xxl-row */}
+			<div className='text-poppins sl-text-head text-nowrap'>
 				Objectifs de
 				<span className='text-primary ms-2'>
 					Charge-in
 				</span>
 			</div>
-			<div className='sl-tab-link list-unstyled d-flex flex-column flex-xl-row'>
-				<div className='d-flex' >
-					<li>
-						<Link to="firsttrim" className={location.pathname==='/home/firsttrim'?
-							'tab_active gradient-lineare-primary h2_regular':
-							'tab_inactive h2_regular bg-secondary'}>
+			<span className=''>
+				<p>
+					<Link to="firsttrim" className={location.pathname==='/home/firsttrim'?
+						'tab_active gradient-lineare-primary h2_regular':
+						'tab_inactive h2_regular bg-secondary'}>
 								1er trimestre
-						</Link>
-					</li>
-					<li>
-						<Link to="secondtrim" className={location.pathname==='/home/secondtrim'?'tab_active gradient-lineare-primary h2_regular':'tab_inactive'}>2ème trimestre</Link>
-					</li>
+					</Link>
+				</p>
+			</span>
+			<span className=''>
+				<Link to="secondtrim" className={location.pathname==='/home/secondtrim'?'tab_active gradient-lineare-primary h2_regular':'tab_inactive'}>2ème trimestre</Link>
+			</span>
+			<span className='text-nowrap'>
+				<Link to="thirdtrim" 
+					className={location.pathname==='/home/thirdtrim'?
+						'tab_active gradient-lineare-primary':
+						'tab_inactive'}>
+							3ème trimestre</Link>
+			</span>
+			<span className='text-nowrap'>
+				<Link to="year2023" className={location.pathname==='/home/year2023'?'tab_active gradient-lineare-primary':'tab_inactive'}>année 2023</Link>	
+			</span>
+			{/** <div className='sl-tab-link list-unstyled d-flex flex-column flex-xl-row'>
+				<div className='d-flex' >
+					
 				</div>
 				<div className='d-flex'>
-					<li>
-						<Link to="thirdtrim" 
-							className={location.pathname==='/home/thirdtrim'?
-								'tab_active gradient-lineare-primary':
-								'tab_inactive'}>
-							3ème trimestre</Link>
-					</li>
-					<li>
-						<Link to="year2023" className={location.pathname==='/home/year2023'?'tab_active gradient-lineare-primary':'tab_inactive'}>année 2023</Link>	
-					</li>
+					
 				</div>						
-			</div>
+			</div> **/}
 		</div>
 		<Outlet/>	
 	</div>;
